2e115a6090a853c8bf2fbc3f05b9f5f63a912917
jussch <jaschultz25@gmail.com>
2015-02-08 16:31:28 -0500

    Starts dynamic objects

---
+++ js/dynamic_object.js
@@ -0,0 +1,21 @@
+(function () {
+  if (window.ZG === undefined) {
+    window.ZG = {};
+  }
+
+  var DynamicObject = ZG.DynamicObject = function (options) {
+    _U.defaultTo(options, {});
+    var defaults = {
+      pos: "missing",
+      vel: "missing",
+      dim: "missing"
+    }
+    _U.extend(defaults, options);
+    _U.extend(this, defaults);
+
+    this.pos = _U.toCoord(this.pos);
+    this.vel = _U.toCoord(this.vel);
+    this.dim = new ZG.CollBox(this.pos, this.dim, {});
+  };
+
+})();
